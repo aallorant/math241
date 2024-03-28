f267ec137b6912cbbdcf755063414307d62f4066
Adrien Allorant <43590555+aallorant@users.noreply.github.com>
2024-03-28 14:29:04 -0700

    adding folder for shinyApps

---
+++ shinyApps/appBabyNames/app.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(tidyverse)
+library(babynames)
+library(DT)
+
+# User interface
+
+ui <- fluidPage()
+
+
+# Server function
+
+server <- function(input, output){
+}
+
+
+# Creates app
+shinyApp(ui = ui, server = server)
